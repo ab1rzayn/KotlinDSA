55863b3de093565f62fd610c6247d799ec41852c
ab1rzayn <160578238+ab1rzayn@users.noreply.github.com>
2024-10-23 05:40:05 +0600

    Add files via upload

---
+++ leetcodeSolve.kt
@@ -0,0 +1,33 @@
+/*
+169. Majority Element
+Easy
+Topics
+Companies
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+ */
+
+fun main(){
+    println(majorityElement(intArrayOf(3,2,3)))
+}
+
+fun majorityElement(nums: IntArray): Int {
+
+    //sorting the arr
+    val sortedArr = nums.sortedArray()
+    return sortedArr[sortedArr.size / 2]
+
+}
