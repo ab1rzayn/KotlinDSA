fcb9f2456b03066bd01214da5cf24ed532d8fb5d
ab1rzayn <160578238+ab1rzayn@users.noreply.github.com>
2024-10-25 07:45:53 +0600

    Add files via upload
    first sorted then apply indexof it .
    At first we are sorting current array . for eg .
    [5,6,6,0,3] = > [0,3,5,6,6]
    
    We apply the map operation where we find the index of each element in the sorted array (sortedNums):
    for 0 -> index is -> 0
    for 3 -> index is -> 1
    for 5 -> index is -> 2
    for 6-> index is -> 3
    for 6 -> index is -> 3[ indexOF takes only unique elements ]
    
    convert the result into the ToIntArray()
    
    Complexity
    Time complexity:
    Total time complexity: O(n2)
    
    Space complexity:

---
+++ leetcode1365.kt
@@ -0,0 +1,32 @@
+//Reverse String LeetCode 344
+
+
+fun main() {
+    val s = charArrayOf('h', 'e', 'l', 'l', 'o')
+    reverseString(s)
+}
+fun reverseString(s: CharArray): Unit {
+    //Two pointer approach
+    var left = 0
+    //size of the array
+    var right = s.size - 1
+
+    //swap the elements till left is less than right
+    //swap the elements at left and right
+    //increment left and decrement right
+    while (left < right) {
+
+        //swap the elements
+        val temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        //increment left and decrement right
+        left++
+        right--
+    }
+
+    //print the reversed string
+    for (ele in s) {
+        print("$ele ")
+    }
+}
